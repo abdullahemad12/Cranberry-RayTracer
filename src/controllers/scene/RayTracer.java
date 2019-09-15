b2d4b9ae593a6d1647ae8fe29c2e455bf40038f3
Abdullah Emad <abdullahem1997@hotmail.com>
2019-09-15 17:09:16 +0200

    Fix design issues

--- src/controllers/scene/RayTracer.java
+++ src/controllers/scene/RayTracer.java
@@ -5,10 +5,13 @@ import exceptions.ColorOverflowException;
 import exceptions.PointOutOfRangeException;
 import model.graphics.Intersection;
 import model.graphics.Ray;
+import model.graphics.Sample;
 import model.graphics.light.Light;
 import model.graphics.object.AggregateShape;
 import model.graphics.object.BRDF;
 import model.graphics.object.Color;
+import model.graphics.object.Shape;
+import model.math.Point;
 import model.math.Vector;
 
 import java.util.List;
@@ -17,22 +20,43 @@ public class RayTracer {
 
     private AggregateShape aggregateShape;
     private List<Light> lights;
-    private int maxDepth;
+    private int maxDepth = 5;
+    private Camera camera;
 
-    public RayTracer(AggregateShape aggregateShape, List<Light> lights, int maxDepth) {
+    public RayTracer(Camera camera) {
+        this.camera = camera;
+    }
+
+    void setAggregateShape(AggregateShape aggregateShape) {
         this.aggregateShape = aggregateShape;
+    }
+
+    void setLights(List<Light> lights) {
         this.lights = lights;
+    }
+
+    void setMaxDepth(int maxDepth) {
         this.maxDepth = maxDepth;
     }
-    Intersection trace(Ray ray) {
+
+    void addToAggregateShape(Shape shape) {
+        aggregateShape.addShape(shape);
+    }
+
+    void addLight(Light light) {
+        this.lights.add(light);
+    }
+
+    Color trace(Sample sample) {
+        Ray ray = camera.generateRay(sample);
         return trace(ray, maxDepth);
     }
 
-    private Intersection trace(Ray ray, int depth) {
+    private Color trace(Ray ray, int depth) {
 
         // no Intersection with any object, return a black color
         if(!aggregateShape.doesIntersect(ray)) {
-            return null;
+            return new Color(0, 0, 0);
         }
         Intersection intersection = null;
         try{
@@ -40,15 +64,14 @@ public class RayTracer {
         } catch (PointOutOfRangeException e) {
             // at this point there must be an implementation error
             // thus print stack trace and terminate the program
-
             e.printStackTrace();
             System.exit(1);
         }
 
         assert(intersection != null);
 
-        // for now return the ambient color of the intersected shape
-        return intersection;
+        // Perform standard Color Shading
+        return performColorShading(intersection);
     }
 
     /**
@@ -56,10 +79,11 @@ public class RayTracer {
      * over all the lights and calculating the phong and lambert terms
      *
      * @param intersection the intersection that was computed perviously
-     * @param eyeDir the direction from the intersection to the eye
      * @return the color produced from vertex Shading
      */
-    Color performColorShading(Intersection intersection, Vector eyeDir) {
+    private Color performColorShading(Intersection intersection) {
+        Point p = intersection.getLocalGeo().getPos();
+        Vector eyeDir = camera.calculateEyeDirection(p);
         BRDF brdf = intersection.getShape().getBRDF();
         Color color = null;
         try {
